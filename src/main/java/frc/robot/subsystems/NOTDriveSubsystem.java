0fd293038c2845ea0abc4ca8dc9c285914472335
mteam88 <matthew8edelen@gmail.com>
2022-10-06 18:40:15 -0400

    The NotBot

---
+++ src/main/java/frc/robot/subsystems/NOTDriveSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NOTDriveSubsystem extends SubsystemBase {
+  TalonSRX lMotor = new TalonSRX(3);
+  TalonSRX rMotor = new TalonSRX(4);
+
+  public void setMotors(double lMotorSpeed, double rMotorSpeed) {
+    lMotor.set(TalonSRXControlMode.PercentOutput, lMotorSpeed*0.25);
+    rMotor.set(TalonSRXControlMode.PercentOutput, rMotorSpeed*0.25);
+  }
+
+  /** Creates a new ExampleSubsystem. */
+  public NOTDriveSubsystem() {
+  
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
