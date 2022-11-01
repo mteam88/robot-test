71885e4434920ae8aa3f1ef10904601abadbcf8e
mteam88 <matthew8edelen@gmail.com>
2022-11-01 19:54:46 -0400

    ultrasonic value recieved

--- src/main/java/frc/robot/subsystems/NOTDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/NOTDriveSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+// import software ultrasonics
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,21 +15,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class NOTDriveSubsystem extends SubsystemBase {
   TalonSRX lMotor = new TalonSRX(3);
   TalonSRX rMotor = new TalonSRX(4);
+  Ultrasonic m_ultrasonic = new Ultrasonic(4, 5);
+  double error = 0;
+  double target = 1000; //mm
+  double k = 0.000001;
+  double speed = 0;
 
   public void setMotors(double lMotorSpeed, double rMotorSpeed, Boolean isSpeedMode) {
-    System.out.println(isSpeedMode);
+    m_ultrasonic.setAutomaticMode(true);
+
+    double dist = m_ultrasonic.getRangeMM();
+    error = target - dist;
+    speed = k * error;
+    
+    System.out.println("Distance: " + dist + "\nSpeed: " + speed);
+
     if (isSpeedMode) {
-      lMotor.set(TalonSRXControlMode.PercentOutput, lMotorSpeed*0.35);
-      rMotor.set(TalonSRXControlMode.PercentOutput, rMotorSpeed*0.35);
+      lMotor.set(TalonSRXControlMode.Velocity, speed);
+      rMotor.set(TalonSRXControlMode.Velocity, speed);
     } else {
-      lMotor.set(TalonSRXControlMode.PercentOutput, lMotorSpeed*0.25);
-      rMotor.set(TalonSRXControlMode.PercentOutput, rMotorSpeed*0.25);
+      lMotor.set(TalonSRXControlMode.Velocity, speed);
+      rMotor.set(TalonSRXControlMode.Velocity, speed);
     }
   }
 
   /** Creates a new ExampleSubsystem. */
   public NOTDriveSubsystem() {
-  
   }
 
   @Override
